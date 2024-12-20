45b437859836db21c7a3285418451a4c3ad38590
Olivier <ogiguere12@gmail.com>
2024-12-20 11:37:03 -0500

    Add unit tests for OrderMatchingEngine and MarketMaker functionality. Switched to pytest

--- trading/tests/market_maker_tests.py
+++ trading/tests/market_maker_tests.py
@@ -1,55 +1,112 @@
 import sys
-sys.path.append('C:/A/ETS/AlgoETS/Simulateur/TradingSystem/TradingSystem/trading')  # Adjust to the actual path of the 'trading' folder
+sys.path.append('C:/Users/ogigu/OneDrive/Documents/Programming/Algo/TradingSystem/trading')  # Adjust to the actual path of the 'trading' folder
+import logging
 
-import unittest
+logging.basicConfig(level=logging.INFO)
+
+import pytest
 from models import *
 
-
-class MarketMaker(unittest.TestCase):
-
-    def setUp(self):
-
-        self.stock_exchange = StockExchange("Quebek")
-
-        self.stock_exchange.addStockMarketListing("AAPL", "Apple Inc", 150.0)
-        self.market_maker = self.stock_exchange.getMarketMaker("AAPL")
-        self.order_matching_engine = self.market_maker.ordermatching_engine
-
-        self.client_buyer = Client("JoeBlow")
-        self.buyer_asset = Assets(money_amount=1000)
+@pytest.fixture
+def setup_environment():
+    """Fixture to set up the testing environment."""
+    stock_exchange = StockExchange("Quebek")
+    stock_exchange.addStockMarketListing("AAPL", "Apple Inc", 150.0)
+    market_maker = stock_exchange.getMarketMaker("AAPL")
+    order_matching_engine = market_maker.ordermatching_engine
 
 
-        self.client_seller = Client("PoorBobby")
-        self.seller_stock = PortfolioStock("AAPL", 10, 120.0,150)
-        self.seller_asset = Assets(self.seller_stock,0)
+    return {
+        "stock_exchange": stock_exchange,
+        "market_maker": market_maker,
+        "order_matching_engine": order_matching_engine,
+    }
 
-    def test_add_buy_order(self):
+@pytest.fixture
+def setup_limit_orders():
+    """Fixture to set up limit orders for testing."""
+    client1_assets = Assets( 5000)
+    client2_assets = Assets(PortfolioStock("AAPL", 30, 140.00, 160.00))
 
-        order = Order("AAPL",100,400,"2021-01-01",Client("ClientName"),True,Assets(PortfolioStock("AAPL", 10, 150.0,120),1000))
-        self.stock_exchange.submit_order(order)
+    client1 = "Client1"
+    client2 = "Client2"
 
-        market_maker = self.stock_exchange.getMarketMaker("AAPL")
-        self.assertIn(order, market_maker.ordermatching_engine.buy_heapq.get_order_list())
+    limit_buy_order = LimitOrder("AAPL", price=155.00, quantity=10, client=client1, buy_order=True, assets=client1_assets)
+    limit_sell_order = LimitOrder("AAPL", price=150.00, quantity=5, client=client2, buy_order=False, assets=client2_assets)
 
-        print(self.order_matching_engine)
+    return {
+        "limit_buy_order": limit_buy_order,
+        "limit_sell_order": limit_sell_order,
+    } 
 
-    def test_match_orders(self):
+@pytest.fixture
+def setup_market_orders():
+    """Fixture to set up limit orders for testing."""
+    client1_assets = Assets( 5000)
+    client2_assets = Assets(PortfolioStock("AAPL", 30, 140.00, 160.00))
 
-        order = Order("AAPL",100,5,"2021-01-01",Client("ClientName"),True,Assets(PortfolioStock("AAPL", 10, 150.0,120),1000))
-        self.stock_exchange.submit_order(order)
+    client1 = "Client1"
+    client2 = "Client2"
 
-        order = Order("AAPL",100,5,"2021-01-01",Client("ClientName"),False,Assets(PortfolioStock("AAPL", 10, 150.0,120),1000))
-        self.stock_exchange.submit_order(order)
+    market_buy_order = MarketOrder("AAPL", price=155.00, quantity=10, client=client1, buy_order=True, assets=client1_assets)
+    market_sell_order = MarketOrder("AAPL", price=150.00, quantity=5, client=client2, buy_order=False, assets=client2_assets)
 
-        self.order_matching_engine.match_orders()
-
-        print(self.order_matching_engine)
-        print(self.stock_exchange.stock_market_listings["AAPL"])
-
-        self.assertEqual(self.order_matching_engine.buy_heapq.get_order_list(),[])
-        self.assertEqual(self.order_matching_engine.sell_heapq.get_order_list(),[])
+    return {
+        "market_buy_order": market_buy_order,
+        "market_sell_order": market_sell_order,
+    } 
 
 
+def test_limit_order(setup_environment,setup_limit_orders):
+    """Test the processing of limit orders."""
+    stock_exchange = setup_environment["stock_exchange"]
+    market_maker = setup_environment["market_maker"]
+    order_matching_engine : OrderMatchingEngine = setup_environment["order_matching_engine"]
+    limit_buy_order = setup_limit_orders["limit_buy_order"]
+    limit_sell_order = setup_limit_orders["limit_sell_order"]
 
-if __name__ == '__main__':
-    unittest.main()
+    market_maker.process_order(limit_buy_order)
+    market_maker.process_order(limit_sell_order)
+
+    order_matching_engine.buy_heapq.size()
+
+    assert order_matching_engine.buy_heapq.size() == 1, "Buy order should be added to the buy orders list."
+    assert order_matching_engine.sell_heapq.size() == 1, "Sell order should be added to the sell orders list."
+    assert order_matching_engine.buy_heapq.peek().price == 155.00, "Buy order should be at the top of the buy orders list."
+    assert order_matching_engine.sell_heapq.peek().price == 150.00, "Sell order should be at the top of the sell orders list."
+
+
+def test_market_buy_order01(setup_environment,setup_market_orders):
+    """Test the processing of market orders."""
+    stock_exchange = setup_environment["stock_exchange"]
+    market_maker : MarketMaker = setup_environment["market_maker"]
+    order_matching_engine : OrderMatchingEngine = setup_environment["order_matching_engine"]
+    market_buy_order = setup_market_orders["market_buy_order"]
+    market_sell_order = setup_market_orders["market_sell_order"]
+    market_maker.volume_sensitivity = 10000
+
+    market_maker.process_order(market_buy_order)
+    market_maker.process_order(market_sell_order)
+
+
+    assert stock_exchange.getStockMarketListing("AAPL").bid_price == 150.00, "Bid price should be updated based on the market order."
+    assert stock_exchange.getStockMarketListing("AAPL").ask_price == 150.00, "Ask price should be updated based on the market order."
+
+
+def test_market_buy_order02(setup_environment,setup_market_orders):
+    """Test the processing of market orders."""
+    stock_exchange = setup_environment["stock_exchange"]
+    market_maker : MarketMaker = setup_environment["market_maker"]
+    order_matching_engine : OrderMatchingEngine = setup_environment["order_matching_engine"]
+    market_buy_order = setup_market_orders["market_buy_order"]
+    market_sell_order = setup_market_orders["market_sell_order"]
+    market_maker.volume_sensitivity = 10000
+
+    market_maker.process_order(market_buy_order)
+    market_maker.process_order(market_sell_order)
+
+
+    assert stock_exchange.getStockMarketListing("AAPL").bid_price == 150.00, "Bid price should be updated based on the market order."
+    
+    
+
